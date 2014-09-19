dbca64e31544ba1d552efa64b56e3ca85e4c3e7e
Roland Senn <rsx@bluewin.ch>
2014-09-19 15:57:54 +0200

    Added additional test cases

--- tests/TestParserBase.hs
+++ tests/TestParserBase.hs
@@ -5,18 +5,38 @@ module TestParserBase where
 
 
 import Test.HUnit
-import ParserBase
+import ParserBase as P
+import Lex as L
 
 
-testsParserBase = TestList [TestLabel "pLit01" pLit01]
+testsParserBase = TestList 
+    [ TestLabel "pLit01" pLit01
+    , TestLabel "pOneOrMore" pOneOrMore01
+    , TestLabel "pOneOreMoreWithSep01" pOneOrMoreWithSep01
+    , TestLabel "pVar01" pVar01
+    , TestLabel "pInt01" pInt01
+    ]
 
 
 pLit01 = TestCase (assertEqual "pLit01"
-              [("hello",["John","!"])]
-              (pLit "hello" ["hello", "John", "!"]))
+    [("hello",["John","!"])]
+    (pLit "hello" (L.lex "hello John!")))
 
+pOneOrMore01 = TestCase (assertEqual "pOneOrMore01"
+    [(["x","x","x","x","x","x"],[])]
+    (pOneOrMore (pLit "x") ["x", "x", "x", "x", "x", "x"]))
 
+pOneOrMoreWithSep01 = TestCase (assertEqual "pOneOrMoreWithSep01"
+    [(["aa","bbb","cccc","dddd","eeee"],[])]
+    (pOneOrMoreWithSep pVar (pLit ",") (L.lex "aa,bbb,cccc,dddd,eeee")))
 
+pVar01 = TestCase (assertEqual "pVar01"
+    [("auto",["bahn"])]
+    (pVar (L.lex "auto bahn")))
+
+pInt01 = TestCase (assertEqual "pInt01"
+    [(2345,["autobahn"])]
+    (pInt (L.lex "2345 autobahn")))
 
 
 
