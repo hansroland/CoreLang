5a602a8b414b9053b7580c667c3845cbd85da97a
Roland Senn <rsx@bluewin.ch>
2014-09-19 09:13:53 +0200

    Added: First version of cabal

---
+++ src/Main.hs
@@ -0,0 +1,12 @@
+-- -----------------------------------------------------------
+-- Main.hs 
+-- -----------------------------------------------------------
+
+-- A preliminary main program 
+
+import Lex
+import Parser
+
+main :: IO()
+main = do
+       putStrLn "This is a dummy version"
